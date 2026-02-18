16da2bddf0d572acec2ba6f5a9e925c28f41e030
Ahmdezjzj <165798198+Ahmdezjzj@users.noreply.github.com>
2026-02-18 01:37:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "--- Starting Gunicorn API Server (in background) ---"
-gunicorn --workers 1 --bind 0.0.0.0:5002 --access-logfile - --error-logfile - api:app &
+gunicorn --workers 1 --bind 0.0.0.0:8080 --access-logfile - --error-logfile - api:app &
 sleep 5
 echo "--- Starting Suwayomi Sync Script (in background) ---"
 python -u suwayomi_sync.py &
